f3b1b52b96c335856f7769fcb936165b6f08ea0d
Chaitanya Yadav <cayadav2009@gmail.com>
2016-06-24 22:18:27 +0530

    Optimization Added

--- app/models/aimage.rb
+++ app/models/aimage.rb
@@ -1,6 +1,8 @@
 class Aimage < ActiveRecord::Base
 	belongs_to :assignment
 	validates :image, presence: true
-  	has_attached_file :image
+  	has_attached_file :image,:styles => {:thumb => "1280*720" },
+										  :convert_options => {
+										    :thumb => "-quality 50 -strip" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
