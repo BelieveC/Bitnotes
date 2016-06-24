74a54432493a450bafe2060de77d60ba06dd3a7d
Chaitanya Yadav <cayadav2009@gmail.com>
2016-06-25 00:02:26 +0530

    Add Optimized Images

--- app/models/eimage.rb
+++ app/models/eimage.rb
@@ -2,6 +2,8 @@ class Eimage < ActiveRecord::Base
 	belongs_to :examnote
 	
 	validates :image, presence: true
-  	has_attached_file :image
+  	has_attached_file :image,:styles => {:thumb => "1280*720" },
+										  :convert_options => {
+										    :thumb => "-quality 50 -strip" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- app/models/qimage.rb
+++ app/models/qimage.rb
@@ -3,7 +3,9 @@ class Qimage < ActiveRecord::Base
 	belongs_to :qpaper
 
 	validates :image, presence: true
-  	has_attached_file :image
+  	has_attached_file :image,:styles => {:thumb => "1280*720" },
+										  :convert_options => {
+										    :thumb => "-quality 50 -strip" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
