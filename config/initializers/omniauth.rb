6e940118213427414717c1b73763ccb85f6ae18c
Chaitanya Yadav <cayadav2009@gmail.com>
2016-06-24 20:07:18 +0530

    Error log Enabled in Production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '985121775306-0mqt6gthqotu02k9jidh45u1iqtitk57.apps.googleusercontent.com', 'mQwniMEV1ZFFo_PROkaRa6ad', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-  provider :facebook, '1150516068300263', 'ecad8eadcf783c855c560a77953f4caf', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  provider :facebook, '490436137808844', '720a669eb670b706b6641b0f7cb6505b', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
