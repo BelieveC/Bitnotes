5a644b60d099d6357badb5089e92c495223c0b51
Chaitanya Yadav <cayadav2009@gmail.com>
2016-06-24 20:11:56 +0530

    Facebook App id Updated

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '985121775306-0mqt6gthqotu02k9jidh45u1iqtitk57.apps.googleusercontent.com', 'mQwniMEV1ZFFo_PROkaRa6ad', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-  provider :facebook, '490436137808844', '720a669eb670b706b6641b0f7cb6505b', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  provider :facebook, '1150506491634554', 'f835fbfdd13a0c669c3ee96dea03c97a', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
