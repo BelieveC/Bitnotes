69d8e52d11dbe94fc4dd74db255ae7f064f2c402
Chaitanya Yadav <cayadav2009@gmail.com>
2016-06-25 21:03:09 +0530

    Model Updated for Stripping images

--- app/models/cimage.rb
+++ app/models/cimage.rb
@@ -2,8 +2,8 @@ class Cimage < ActiveRecord::Base
 	belongs_to :college
 	
 	validates :image, presence: true
-  	has_attached_file :image,:styles => {:thumb => "1280*720" },
+  	has_attached_file :image,:styles => {:thumb => "1280*720>",:medium=>"640*426#",:small=>"180*320#"},
 										  :convert_options => {
-										    :thumb => "-quality 50 -strip" }
+										    :thumb => "-quality 50 -strip",:medium=>"-quality 60 -strip",:small=>"-quality 60 -strip" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
