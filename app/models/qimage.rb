d712a113d8ef38055c6ee8aa587586cc5eb1b4b5
Chaitanya Yadav <cayadav2009@gmail.com>
2016-06-25 22:14:08 +0530

    Index and Show Pages Updated

--- app/models/qimage.rb
+++ app/models/qimage.rb
@@ -5,7 +5,7 @@ class Qimage < ActiveRecord::Base
 	validates :image, presence: true
   	has_attached_file :image,:styles => {:thumb => "1280*720>",:medium=>"640*426#",:small=>"180*320#"},
 										  :convert_options => {
-										    :thumb => "-quality 50 -strip",:medium=>"-quality 60 -strip",:small=>"-quality 60 -strip" }
+										    :thumb => "-quality 50 -strip",:medium=>"-quality 60 -strip",:small=>"-quality 90 -strip" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
