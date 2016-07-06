eeca07598e50f103bcaf526099ebc37c14767a2f
Chaitanya Yadav <cayadav2009@gmail.com>
2016-07-06 19:09:19 +0530

    User model Updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+
+	validates :avatar, presence: true
+  	has_attached_file :avatar,:styles => {:thumb => "1280*720>",:medium=>"640*426#",:small=>"180*320#"},
+										  :convert_options => {
+										    :thumb => "-quality 50 -strip",:medium=>"-quality 60 -strip",:small=>"-quality 90 -strip" }
+  	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 	def self.from_omniauth(auth)
 	    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
 	      user.provider = auth.provider
