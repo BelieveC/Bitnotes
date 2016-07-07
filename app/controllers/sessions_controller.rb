e356e3292642573345123f90641e509310edf3c6
Chaitanya Yadav <cayadav2009@gmail.com>
2016-07-07 22:09:45 +0530

    Sessions controller changed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 	def create
 		user = User.from_omniauth(env["omniauth.auth"])
 	    session[:user_id] = user.id
-	    redirect_to root_path,notice:" #{user.name} you are logged in. Have a great Time at Bitnotes."
+	    redirect_to user,notice:" #{user.name} you are logged in. Have a great Time at Bitnotes."
 	end
 
 	def destroy
