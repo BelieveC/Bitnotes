c32acbfd5ce9cc24c8ce027fea3c9cbc6f232cf3
Chaitanya Yadav <cayadav2009@gmail.com>
2016-07-08 21:53:30 +0530

    User Controller added with Authentication for Editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :get_user, only:[:show,:edit,:update,:destroy]
-
+  before_action :authenticate_user!,only:[:edit,:update,:destroy]
 	def index
 	end
 
@@ -15,20 +15,32 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		render layout: "form"
+		if session[:user_id] == @user.id
+			render layout: "form"
+		else
+			redirect_to root_path,"Gotcha!,You are not owner of this Profile."
+		end
 	end
 
 	def update
-		if @user.update(user_params)
-			redirect_to @user,notice:"Publishing House Info Updated Successfully"
+		if session[:user_id] == @user.id
+			if @user.update(user_params)
+				redirect_to @user,notice:"Profile Info Updated Successfully"
+			else
+				render "edit",layout: "form"
+			end
 		else
-			render "edit",layout: "form"
+			redirect_to @user,notice:"Gotcha!, You are not Owner of this Profile"
 		end
 	end
 
 	def destroy
-		@user.destroy
-		redirect_to root_page,notice: "Your Publishing House Closed Successfully!"
+		if session[:user_id] == @user.id
+			@user.destroy
+			redirect_to root_page,notice: "Your Publishing House Closed Successfully!"
+		else
+			redirect_to root_path,notice: "Gotcha!, You are not owner of this Profile"
+		end
 	end
 
 	private
