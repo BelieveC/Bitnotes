7e8cd2826fa4c3f4e52be2d5598671d10ae392f4
Chaitanya Yadav <cayadav2009@gmail.com>
2016-07-09 23:56:59 +0530

    Migration Changes to Remove Subject Caching vote

--- db/migrate/20160707073649_add_cache_to_subject.rb
+++ db/migrate/20160707073649_add_cache_to_subject.rb
@@ -1,16 +1,5 @@
 class AddCacheToSubject < ActiveRecord::Migration
    def self.up
-    add_column :subjects, :cached_votes_total, :integer, :default => 0
-    add_column :subjects, :cached_votes_score, :integer, :default => 0
-    add_column :subjects, :cached_votes_up, :integer, :default => 0
-    add_column :subjects, :cached_votes_down, :integer, :default => 0
-    add_index  :subjects, :cached_votes_total
-    add_index  :subjects, :cached_votes_score
-    add_index  :subjects, :cached_votes_up
-    add_index  :subjects, :cached_votes_down
-
-    # Uncomment this line to force caching of existing votes
-    Subject.find_each(&:update_cached_votes)
 
     add_column :colleges, :cached_votes_total, :integer, :default => 0
     add_column :colleges, :cached_votes_score, :integer, :default => 0
@@ -26,10 +15,6 @@ class AddCacheToSubject < ActiveRecord::Migration
   end
 
   def self.down
-    remove_column :subjects, :cached_votes_total
-    remove_column :subjects, :cached_votes_score
-    remove_column :subjects, :cached_votes_up
-    remove_column :subjects, :cached_votes_down
 
     remove_column :colleges, :cached_votes_total
     remove_column :colleges, :cached_votes_score
