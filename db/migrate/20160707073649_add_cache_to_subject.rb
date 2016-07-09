e53c10ab4316e32db184a25f3c2277c7a029956b
Chaitanya Yadav <cayadav2009@gmail.com>
2016-07-09 23:59:39 +0530

    College votes caching Removed

--- db/migrate/20160707073649_add_cache_to_subject.rb
+++ db/migrate/20160707073649_add_cache_to_subject.rb
@@ -1,24 +1,7 @@
 class AddCacheToSubject < ActiveRecord::Migration
    def self.up
-
-    add_column :colleges, :cached_votes_total, :integer, :default => 0
-    add_column :colleges, :cached_votes_score, :integer, :default => 0
-    add_column :colleges, :cached_votes_up, :integer, :default => 0
-    add_column :colleges, :cached_votes_down, :integer, :default => 0
-    add_index  :colleges, :cached_votes_total
-    add_index  :colleges, :cached_votes_score
-    add_index  :colleges, :cached_votes_up
-    add_index  :colleges, :cached_votes_down
-
-    # Uncomment this line to force caching of existing votes
-    College.find_each(&:update_cached_votes)
   end
 
   def self.down
-
-    remove_column :colleges, :cached_votes_total
-    remove_column :colleges, :cached_votes_score
-    remove_column :colleges, :cached_votes_up
-    remove_column :colleges, :cached_votes_down
    end
 end
