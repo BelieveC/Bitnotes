f92ac0da7d421c123cba35a6c4ad9d1f7f88a43e
Chaitanya Yadav <cayadav2009@gmail.com>
2016-07-10 01:24:22 +0530

    User model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
 	      user.provider = auth.provider
 	      user.uid = auth.uid
 	      user.name = auth.info.name
+	      user.phname = auth.info.name
 	      user.oauth_token = auth.credentials.token
 	      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
 	      user.save!
