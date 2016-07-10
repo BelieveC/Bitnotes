3e6033d46edf4524a63aa6f075b0b3d56d46a624
Chaitanya Yadav <cayadav2009@gmail.com>
2016-07-10 07:39:42 +0530

    Remove searchkick from Practical

--- app/models/practical.rb
+++ app/models/practical.rb
@@ -1,6 +1,5 @@
 class Practical < ActiveRecord::Base
 	is_impressionable
-	searchkick
 	acts_as_votable
 	has_many :pimages
 	accepts_nested_attributes_for :pimages, reject_if: :all_blank, allow_destroy: true
