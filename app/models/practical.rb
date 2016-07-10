509834fe8a4a1b9038f37e71da93daf45db5c116
Chaitanya Yadav <cayadav2009@gmail.com>
2016-07-10 07:43:31 +0530

    Add Searchkick back to Practicals

--- app/models/practical.rb
+++ app/models/practical.rb
@@ -1,6 +1,7 @@
 class Practical < ActiveRecord::Base
 	is_impressionable
 	acts_as_votable
+	searchkick
 	has_many :pimages
 	accepts_nested_attributes_for :pimages, reject_if: :all_blank, allow_destroy: true
 end
