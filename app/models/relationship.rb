05e01ed32a4e3fb38b0720e4667abeefdc29d763
Chaitanya Yadav <cayadav2009@gmail.com>
2016-07-11 12:24:51 +0530

    Add Validation to Relationship Model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,8 @@ class Relationship < ActiveRecord::Base
 
 	belongs_to :subscribed, class_name: "User"
 	belongs_to :subscriber, class_name: "User"
+
+	validates :subscriber_id, presence: true
+	validates :subscribed_id, presence: true
+	
 end
