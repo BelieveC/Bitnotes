0bf5530291589211e5480382e5da0fe5aff92ba2
Chaitanya Yadav <cayadav2009@gmail.com>
2016-07-11 13:19:16 +0530

    COnfigure Relationships Controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,18 @@
 class RelationshipsController < ApplicationController
+
+	before_action :authenticate_user!
+
+	def create
+		user = User.find(params[:followed_id])
+		current.user.subscribe(user)
+		redirect_to :back
+	end
+
+	def destroy
+		user = Relationship.find(params[:id]).followed
+    	current_user.unfollow(user)
+    	redirect_to user
+	end
+
 end
+
