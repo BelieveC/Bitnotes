d28725eb51c667fa33c71b2b454a8b3195f1b5c7
Chaitanya Yadav <cayadav2009@gmail.com>
2016-07-13 13:59:23 +0530

    Fix Practical Subscribe Bug

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,8 +3,8 @@ class RelationshipsController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-		user = User.find(params[:subscribed_id])
-		current_user.active_relationships.create(subscribed_id: user.id)
+		@user = User.find(params[:subscribed_id])
+		current_user.active_relationships.create(subscribed_id: @user.id)
 		respond_to do |format|
 			format.html{redirect_to :back}
 			format.js
@@ -12,8 +12,8 @@ class RelationshipsController < ApplicationController
 	end
 
 	def destroy
-		user = Relationship.find(params[:id]).subscribed
-    	current_user.active_relationships.find_by(subscribed_id: user.id).destroy
+		@user = Relationship.find(params[:id]).subscribed
+    	current_user.active_relationships.find_by(subscribed_id: @user.id).destroy
     	respond_to do |format|
     		format.html{redirect_to :back}
     		format.js
