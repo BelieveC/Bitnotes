054a47d28fc3641409d937610a7ab4b33bdadfb6
BelieveC <cayadav2009@gmail.com>
2016-08-12 19:38:09 +0530

    Cleanup Images folder and Add customcover image on abouts  and college index

--- app/models/aimage.rb
+++ app/models/aimage.rb
@@ -1,8 +1,8 @@
 class Aimage < ActiveRecord::Base
 	belongs_to :assignment
 	validates :image, presence: true
-  	has_attached_file :image,:styles => {:thumb => "1280*720>",:medium=>"640*426#",:small=>"180*320#"},
+  	has_attached_file :image,:styles => {:thumb => "1280*720>",:medium=>"640*426#",:small=>"250*160#"},
 										  :convert_options => {
-										    :thumb => "-quality 50 -strip",:medium=>"-quality 60 -strip",:small=>"-quality 90 -strip" }
+										    :thumb => "-quality 50 -strip",:medium=>"-quality 60 -strip",:small=>"-quality 100 -strip" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- app/models/eimage.rb
+++ app/models/eimage.rb
@@ -2,8 +2,8 @@ class Eimage < ActiveRecord::Base
 	belongs_to :examnote
 	
 	validates :image, presence: true
-  	has_attached_file :image,:styles => {:thumb => "1280*720>",:medium=>"640*426#",:small=>"180*320#"},
+  	has_attached_file :image,:styles => {:thumb => "1280*720>",:medium=>"640*426#",:small=>"250*160#"},
 										  :convert_options => {
-										    :thumb => "-quality 50 -strip",:medium=>"-quality 60 -strip",:small=>"-quality 90 -strip" }
+										    :thumb => "-quality 50 -strip",:medium=>"-quality 60 -strip",:small=>"-quality 100 -strip" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
