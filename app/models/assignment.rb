0ef389affb6c4d8106e3efb921425da5987539fb
BelieveC <cayadav2009@gmail.com>
2016-08-14 13:12:00 +0530

    Add Validation to Assignment and College Model

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -7,4 +7,10 @@ class Assignment < ActiveRecord::Base
 	belongs_to :subject
 	has_many :aimages
 	accepts_nested_attributes_for :aimages, reject_if: :all_blank, allow_destroy: true
+
+
+	validates :topic, presence: true,length:{minimum: 5}
+	validates :college_id, presence: true
+	validates :subject_id, presence: true
+	validates :description, presence: true, length:{minimum: 20}
 end
--- app/models/college.rb
+++ app/models/college.rb
@@ -9,4 +9,7 @@ class College < ActiveRecord::Base
 	belongs_to :user
 	has_many :cimages
 	accepts_nested_attributes_for :cimages, reject_if: :all_blank, allow_destroy: true
+
+	validates :name, presence: true,length:{minimum: 5}
+	validates :address, presence: true, length:{minimum: 10}
 end
