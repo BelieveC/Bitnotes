a349f5fc50c9cc08dab4d75a7903ff2f66fd2627
BelieveC <cayadav2009@gmail.com>
2016-08-15 09:36:36 +0530

    Fix Subscribe ajax bug

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,6 +3,7 @@ class RelationshipsController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
+		@form_id = params[:form_id]
 		@user = User.find(params[:subscribed_id])
 		current_user.active_relationships.create(subscribed_id: @user.id)
 		respond_to do |format|
@@ -12,6 +13,7 @@ class RelationshipsController < ApplicationController
 	end
 
 	def destroy
+		@form_id = params[:form_id]
 		@user = Relationship.find(params[:id]).subscribed
     	current_user.active_relationships.find_by(subscribed_id: @user.id).destroy
     	respond_to do |format|
