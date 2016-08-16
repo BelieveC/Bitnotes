11481163886c69f6720cc53044b752c72c9ef5de
BelieveC <cayadav2009@gmail.com>
2016-08-16 17:29:34 +0530

    Fix user error and add opacity to top-button

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-	searchkick
 	acts_as_voter
 	has_many :active_relationships, class_name: "Relationship",
 									foreign_key: "subscriber_id",
@@ -38,13 +37,13 @@ class User < ActiveRecord::Base
 
 
 	def self.from_omniauth(auth)
-	    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+	    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
 	      user.provider = auth.provider
 	      user.uid = auth.uid
 	      user.name = auth.info.name
 	      user.phname = auth.info.name
-	      user.intro = "hello Welcome to my Publication"
-	      user.description = "Welcome to my Publication-"
+	      user.intro = "Welcome to my Publication"
+	      user.description = "Welcome to my Publication"
 	      user.oauth_token = auth.credentials.token
 	      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
 	      user.save!
