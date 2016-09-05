0463a163e1ee36630cf22fa9a0b0efad76805d92
BelieveC <cayadav2009@gmail.com>
2016-09-05 12:50:32 +0530

    Add Controllers for Practical APi

---
+++ app/controllers/v1/practicals_controller.rb
@@ -0,0 +1,51 @@
+module V1
+	class PracticalsController < ApiController
+		before_action :get_practical, only:[:show]
+
+		def index
+			@practicals = Practical.all
+
+			count = 0
+			images = Array.new
+			@practicals.each do |practical|
+				if practical.pimages.count > 0
+					images[count] = practical.pimages.first.image.url(:medium)
+				else
+					images[count] = nil
+				end
+			end
+
+			render status: :ok,json: {
+				practicals: @practicals,
+				images: images
+			}
+		end
+
+
+		def show
+
+			images = Array.new
+			count = 0
+			if @practical.pimages.count > 0
+				@practical.pimages.each do |pimage|
+					images[count] = pimage.image.url(:medium)
+					count = count + 1
+				end
+			else
+				images[count] = nil
+			end
+
+			render status: :ok, json:{
+				practical: @practical,
+				images: images
+			}
+		end
+
+
+		private
+
+			def get_practical
+				@practical = Practical.find(params[:id])
+			end
+	end
+end
