92a9fc2887a0474282563991870151b8c3057f17
BelieveC <cayadav2009@gmail.com>
2016-09-06 03:00:31 +0530

    Add helper for Dynamic Meta Data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
 	 def title(page_title)
 	    content_for :title , page_title.to_s
 	 end
+
+	 def keywords(keyword)
+	 	content_for :keywords, keyword.to_s
+	 end
+
+	 def description(description)
+	 	content_for :description, description.to_s
+	 end
+
+	 def author(author)
+	 	content_for :author, author.to_s
+	 end
 end
