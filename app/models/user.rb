5ff124f100fc37aa4169c175012e2c0cac3bfe30
BelieveC <cayadav2009@gmail.com>
2016-09-23 00:26:09 +0530

    Add Logo image and Add email field to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,6 +49,7 @@ class User < ActiveRecord::Base
     # Omniauth
 	def self.from_omniauth(auth)
 	    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	      user.email = auth.info.email
 	      user.provider = auth.provider
 	      user.uid = auth.uid
 	      user.name = auth.info.name
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '985121775306-d93srhn0iauribnri811mm6bi86211de.apps.googleusercontent.com', '7qn4dFdFJbbTt9TFDXqRUPEI', {client_options: {ssl: {ca_file: Rails.root.join("cacert.crt").to_s}}}
-  provider :facebook, '490436137808844', '720a669eb670b706b6641b0f7cb6505b', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.crt").to_s}}}
+  provider :facebook, '490436137808844', '720a669eb670b706b6641b0f7cb6505b', {:scope => 'email', :display => 'popup', :info_fields => 'name,email',:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.crt").to_s}}}
 end
