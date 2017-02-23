9c82e47bb46da9becb544b58774a9a82437760f6
Chaitanya <cayadav2009@gmail.com>
2017-02-24 03:44:45 +0530

    Change Structure of Subject API

--- app/controllers/v1/subjects_controller.rb
+++ app/controllers/v1/subjects_controller.rb
@@ -6,49 +6,12 @@ module V1
 
 		def index
 			@subjects = Subject.recent.limit(12)
-			count = 0
-			images = Array.new
-			views = Array.new
-			publications = Array.new
-			@subjects.each do |subject|
-				views[count] = subject.impressionist_count
-				publications[count] = subject.user.phname
-				if subject.simages.count > 0
-					images[count] = subject.simages.first.image.url(:medium)
-				else
-					images[count] = nil
-				end
-				count = count + 1
-			end
-
-			render status: :ok,json: {
-				subjects: @subjects,
-				images: images,
-				publications: publications,
-				views: views
-			}
+			render json: @subjects.as_json(include: { user: { only: [:phname] }, simages: { methods: [:image_url], only: [] } }), status: :ok
 		end
 
 
 		def show
-
-			images = Array.new
-			count = 0
-			if @subject.simages.count > 0
-				@subject.simages.each do |simage|
-					images[count] = simage.image.url(:thumb)
-					count = count + 1
-				end
-			else
-				images[count] = nil
-			end
-
-			render status: :ok, json:{
-				subject: @subject,
-				publication: @subject.user.phname,
-				views: @subject.impressionist_count,
-				images: images
-			}
+			render json: @subject.as_json(include: { user: { only: [:phname] }, simages: { methods: [:image_url], only: [] } }), status: :ok
 		end
 
 		def upvote
