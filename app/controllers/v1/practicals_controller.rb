0cdc072eb210dd01251fe68fc48a24c74b78bbd7
Chaitanya <cayadav2009@gmail.com>
2017-02-24 03:15:54 +0530

    Change Structure of Practical API

--- app/controllers/v1/practicals_controller.rb
+++ app/controllers/v1/practicals_controller.rb
@@ -6,49 +6,12 @@ module V1
 
 		def index
 			@practicals = Practical.recent.limit(12)
-			count = 0
-			images = Array.new
-			views = Array.new
-			publications = Array.new
-			@practicals.each do |practical|
-				views[count] = practical.impressionist_count
-				publications[count] = practical.user.phname
-				if practical.pimages.count > 0
-					images[count] = practical.pimages.first.image.url(:medium)
-				else
-					images[count] = nil
-				end
-				count = count + 1
-			end
-
-			render status: :ok,json: {
-				practicals: @practicals,
-				images: images,
-				publications: publications,
-				views: views
-			}
+			render json: @practicals.as_json( include: { user: { only: [:phname] }, pimages: { methods: [:image_url], only:[] } } ),status: :ok
 		end
 
 
 		def show
-
-			images = Array.new
-			count = 0
-			if @practical.pimages.count > 0
-				@practical.pimages.each do |pimage|
-					images[count] = pimage.image.url(:thumb)
-					count = count + 1
-				end
-			else
-				images[count] = nil
-			end
-
-			render status: :ok, json:{
-				practical: @practical,
-				publication: @practical.user.phname,
-				views: @practical.impressionist_count,
-				images: images
-			}
+			render json: @practical.as_json( include: { user: { only: [:phname] }, pimages: { methods: [:image_url], only:[] } } ),status: :ok
 		end
 
 		def upvote
